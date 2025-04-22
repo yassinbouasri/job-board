2b4f2d8f02a6dfce553018e58067203461c889e3
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-04-23 01:18:16 +0200

    Crud for job entity

---
+++ src/Controller/JobController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Job;
+use App\Form\JobType;
+use App\Repository\JobRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/job')]
+final class JobController extends AbstractController{
+    #[Route(name: 'app_job_index', methods: ['GET'])]
+    public function index(JobRepository $jobRepository): Response
+    {
+        return $this->render('job/index.html.twig', [
+            'jobs' => $jobRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_job_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $job = new Job();
+        $form = $this->createForm(JobType::class, $job);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($job);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_job_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('job/new.html.twig', [
+            'job' => $job,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_job_show', methods: ['GET'])]
+    public function show(Job $job): Response
+    {
+        return $this->render('job/show.html.twig', [
+            'job' => $job,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_job_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Job $job, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(JobType::class, $job);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_job_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('job/edit.html.twig', [
+            'job' => $job,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_job_delete', methods: ['POST'])]
+    public function delete(Request $request, Job $job, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$job->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($job);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_job_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/JobType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Job;
+use App\Entity\user;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class JobType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('location')
+            ->add('salary')
+            ->add('tags')
+            ->add('created_at', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('createdBy', EntityType::class, [
+                'class' => user::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Job::class,
+        ]);
+    }
+}
