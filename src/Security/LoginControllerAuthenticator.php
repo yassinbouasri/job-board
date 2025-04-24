512702c172f727883991e5c34ac0f3c12c09bad9
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-04-25 01:51:02 +0200

    redirecting after login

--- src/Security/LoginControllerAuthenticator.php
+++ src/Security/LoginControllerAuthenticator.php
@@ -28,12 +28,12 @@ class LoginControllerAuthenticator extends AbstractLoginFormAuthenticator
 
     public function authenticate(Request $request): Passport
     {
-        $companyName = $request->getPayload()->getString('company_name');
+        $email = $request->getPayload()->getString('email');
 
-        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $companyName);
+        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
 
         return new Passport(
-            new UserBadge($companyName),
+            new UserBadge($email),
             new PasswordCredentials($request->getPayload()->getString('password')),
             [
                 new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
@@ -49,8 +49,8 @@ class LoginControllerAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+         return new RedirectResponse($this->urlGenerator->generate('app_job_index'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
