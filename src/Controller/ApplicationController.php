c45e5743f09f39b9abb1fb1f714c403b9d1683ed
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-05-01 02:25:47 +0200

    application logic

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -15,21 +15,41 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ApplicationController extends AbstractController
 {
     #[Route('/job/{id}/apply', name: 'app_apply', methods: ['GET', 'POST'])]
-    public function apply(Request $request ,Job $job, EntityManagerInterface $entityManager): Response
+    public function apply(Request $request ,Job $job, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $application  =  new Application();
         $user = $this->getUser();
 
         $form = $this->createForm(ApplicationType::class, $application);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
 
-            $application->setApplicant($user);
+        if ($form->isSubmitted() && $form->isValid()) {
             $application->setJob($job);
+            $application->setApplicant($user);
+            $resume = $form->get('resume')->getData();
+            if ($resume) {
+                try {
+                    $originalFilename = pathinfo($resume->getClientOriginalName(), PATHINFO_FILENAME);
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename.'-'.uniqid().'.'.$resume->guessExtension();
+
+                    $resume->move($this->getParameter('resume_directory'), $newFilename);
+
+                    $application->setResume($newFilename);
+                }catch (\Exception $exception){
+                    $this->addFlash('error', $exception->getMessage());
+                }
+
+            }
+
+            $application->setCoverLetter($form->get('coverLetter')->getData());
+
+
 
             $entityManager->persist($application);
             $entityManager->flush();
@@ -40,6 +60,7 @@ class ApplicationController extends AbstractController
         return $this->render('application/apply.html.twig', [
             'job' => $job,
             'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
