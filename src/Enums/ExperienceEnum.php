30133942b9aed469f2ee2528d9d02ad32911ed9a
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-05-05 16:10:58 +0200

    implemented ExperienceEnum.php

---
+++ src/Enums/ExperienceEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum ExperienceEnum: string
+{
+    case ENTRY_LEVEL = 'Entry Level';
+    case MID_LEVEL = 'Mid Level';
+    case SENIOR_LEVEL = 'Senior Level';
+}
