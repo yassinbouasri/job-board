d04d1b43817f13914ceb1a4072a3b2517c47d148
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-05-07 01:56:38 +0200

    using only one standard function for all enums, and remove duplication

---
+++ src/Service/EnumValues.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Service;
+
+use App\Enums\ExperienceEnum;
+use App\Enums\JobTypeEnum;
+use Symfony\Component\HttpFoundation\Request;
+
+class EnumValues
+{
+    public static function getEnum(Request $request, $enum,string $formName, string $queryParam): mixed
+    {
+        $queryParams = $request->query->all();
+
+        $selectedValue = $queryParams[$formName][$queryParam] ?? null;
+
+        return $selectedValue ? $enum::from($selectedValue) : null;
+    }
+}
