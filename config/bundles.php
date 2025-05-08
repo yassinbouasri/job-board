fbe051c1d047585cc9e01b1ee2bde2374f16954a
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-05-09 01:32:07 +0200

    require easyAdmin bundle

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
