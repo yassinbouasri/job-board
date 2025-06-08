21cad214844d93419ad48dbdfad13575b837234b
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-06-08 15:47:11 +0100

    JobALert controller new JobAlert()

--- src/Controller/JobAlertController.php
+++ src/Controller/JobAlertController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\JobAlert;
+use App\Entity\User;
 use App\Form\JobAlertFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,13 +13,18 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class JobAlertController extends AbstractController{
     #[Route('/job/alert', name: 'app_job_alert')]
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
         $jobAlert = new JobAlert();
 
-        $jobAlert->addUsr($this->getUser());
+        /** @var User $user */
+        $user = $this->getUser();
 
-        $form = $this->createForm(JobAlertFormType::class);
+        $jobAlert->addUsr($user);
+        $jobAlert->setTags([]);
+
+
+        $form = $this->createForm(JobAlertFormType::class, $jobAlert);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
