9187a4145b84ac50cecae653f337db9b737ba9ad
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-06-10 11:53:45 +0100

    migration

---
+++ migrations/Version20250609235553.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250609235553 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE job ALTER COLUMN tags TYPE JSON USING to_json(tags)
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN job.tags IS NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE job ALTER tags TYPE TEXT
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN job.tags IS '(DC2Type:array)'
+        SQL);
+    }
+}
