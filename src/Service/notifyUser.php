d3f57e3ae2b2e8eddbeb021a50004c5c1e15759b
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-06-13 00:45:01 +0100

    fixed notifyUser.php service

--- src/Service/notifyUser.php
+++ src/Service/notifyUser.php
@@ -6,13 +6,33 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\User;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 
 class notifyUser
 {
-    public static function jobNotification(array $jobs, User $user, MailerInterface $mailer): void
+    public static function jobNotification(array $jobs, User $user, MailerInterface $mailer, EntityManagerInterface $entityManager): void
     {
+        if (empty($jobs)) {
+            return;
+        }
+        $alerts = $user->getJobAlerts();
+
+        if (empty($alerts)) {
+            return;
+        }
+
+        foreach ($alerts as $alert) {
+            if ($alert->getLastNotifiedAt() === null){
+                continue;
+            }
+            if ($alert->getLastNotifiedAt() <= new \DateTimeImmutable('now')) {
+                return;
+            }
+        }
+
         $email = (new TemplatedEmail());
         $email->from('info@job-board.com');
         $email->to($user->getEmail());
@@ -24,6 +44,11 @@ class notifyUser
         $email->htmlTemplate('email/notify_user.html.twig');
         $mailer->send($email);
 
-    }
+        foreach ($alerts as $alert) {
+            $alert->setLastNotifiedAt(new \DateTimeImmutable('now'));
+            $entityManager->flush($alert);
 
+        }
+
+    }
 }
