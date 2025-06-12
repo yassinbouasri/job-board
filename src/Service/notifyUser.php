952a815a8345d62b533d550f125e785853cb952b
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-06-13 00:49:14 +0100

    refactor; extract email logic

--- src/Service/notifyUser.php
+++ src/Service/notifyUser.php
@@ -9,10 +9,14 @@ use App\Entity\User;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 
 class notifyUser
 {
+    /**
+     * @throws TransportExceptionInterface
+     */
     public static function jobNotification(array $jobs, User $user, MailerInterface $mailer, EntityManagerInterface $entityManager): void
     {
         if (empty($jobs)) {
@@ -33,16 +37,7 @@ class notifyUser
             }
         }
 
-        $email = (new TemplatedEmail());
-        $email->from('info@job-board.com');
-        $email->to($user->getEmail());
-        $email->subject('Job Board - Notification');
-        $email->context([
-            'jobs' => $jobs,
-            'user' => $user,
-        ]);
-        $email->htmlTemplate('email/notify_user.html.twig');
-        $mailer->send($email);
+        self::sendMailNotification($user, $jobs, $mailer);
 
         foreach ($alerts as $alert) {
             $alert->setLastNotifiedAt(new \DateTimeImmutable('now'));
@@ -50,5 +45,28 @@ class notifyUser
 
         }
 
+    }
+
+    /**
+     * @param  User  $user
+     * @param  array  $jobs
+     * @param  MailerInterface  $mailer
+     * @return void
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
+    private static function sendMailNotification(User $user, array $jobs, MailerInterface $mailer): void
+    {
+        $email = (new TemplatedEmail());
+        $email->from('info@job-board.com');
+        $email->to($user->getEmail());
+        $email->subject('Job Board - Notification');
+        $email->context(
+            [
+                'jobs' => $jobs,
+                'user' => $user,
+            ]
+        );
+        $email->htmlTemplate('email/notify_user.html.twig');
+        $mailer->send($email);
     }
 }
