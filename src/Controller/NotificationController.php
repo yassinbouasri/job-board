898ef116b0500c73a9743c04c1a42ed83ee1cb04
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-06-17 16:04:49 +0100

    mark notification as read

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Notification;
 use App\Entity\User;
 use App\Service\MatchedJobsPreferences;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("ROLE_USER")]
 final class NotificationController extends AbstractController{
     #[Route('/notification', name: 'app_notifications')]
     public function index(EntityManagerInterface $entityManager): Response
@@ -23,8 +26,15 @@ final class NotificationController extends AbstractController{
         ]);
     }
 
-    public function markAsRead(): Response
+    #[Route('/mark-as-read/{id}', name: 'notification_mark_read', methods: ['POST'])]
+    public function markAsRead(Response $request, Notification $notification, EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
 
+        $notification->setIsRead(true);
+        $entityManager->persist($notification);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_notifications');
     }
 }
